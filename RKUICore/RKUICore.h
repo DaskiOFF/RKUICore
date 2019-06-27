7a22ef8fb0be72424170585ceeb8b5fbd0393d5c
Roman Kotov <waydeveloper@gmail.com>
2019-06-27 09:33:42 +0300

    Initial Commit

---
+++ RKUICore/RKUICore.h
@@ -0,0 +1,19 @@
+//
+//  RKUICore.h
+//  RKUICore
+//
+//  Created by Roman Kotov on 27/06/2019.
+//  Copyright © 2019 Roman Kotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKUICore.
+FOUNDATION_EXPORT double RKUICoreVersionNumber;
+
+//! Project version string for RKUICore.
+FOUNDATION_EXPORT const unsigned char RKUICoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKUICore/PublicHeader.h>
+
+
